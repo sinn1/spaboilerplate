5b46e9640703249d4f1cd9c951adbd73c67467c2
sinn1 <joel.chan1@gmail.com>
2014-04-07 21:19:50 +0100

    more boilerplate

--- SPABoilerplate/Controllers/DataController.cs
+++ SPABoilerplate/Controllers/DataController.cs
@@ -12,5 +12,16 @@ namespace SPABoilerplate.Controllers
         public DataController(IDataStore dataStore) : base(dataStore)
         {
         }
+
+        public override IEnumerable<Data> GetAll()
+        {
+            return new List<Data>()
+            {
+                new Data() { Id = 1, Name = "Data1"},
+                new Data() { Id = 2, Name = "Data2"},
+                new Data() { Id = 3, Name = "Data3"},
+                new Data() { Id = 4, Name = "Data4"}
+            };
+        }
     }
 }
